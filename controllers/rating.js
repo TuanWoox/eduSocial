aa113983fd8ae93244beab8c8154dfae8672fb28
TuanWoox <z1venttt@gmail.com>
2024-12-09 13:05:39 +0700

    Add notification for replyto and course

--- controllers/rating.js
+++ controllers/rating.js
@@ -1,14 +1,30 @@
 const Rating = require('../models/rating');
-
+const Notification = require('../models/notification');
+const Course = require('../models/course');
 module.exports.saveRating = async (req,res) => {
     const newRating = new Rating(req.body);
     newRating.courseRated = req.params.id;
     newRating.author = req.user._id;
     await newRating.save();
+    const course = await Course.findById(req.params.id).populate('author');
+    if (course && !course.author.equals(req.user._id)) {
+        // Tạo thông báo cho chủ sở hữu bài viết
+        const courseNotification = new Notification({
+            recipient: course.author._id,
+            sender: req.user._id,
+            course: req.params.id,
+            rating: newRating.id,
+            message: `${req.user.name} đã đánh giá về khóa học của bạn.`,
+            isRead: false,
+        });
+        await courseNotification.save();
+    }
     res.redirect(`/courses/${req.params.id}`);
 }
 module.exports.deleteRating = async (req,res) => {
     const deletedRating = await Rating.findByIdAndDelete(req.params.ratingID);
-    console.log(deletedRating);
+    await Notification.findOneAndDelete({
+        rating: req.params.ratingID, 
+    });
     res.redirect(`/courses/${req.params.id}`);
 }
--- models/notification.js
+++ models/notification.js
@@ -7,7 +7,7 @@ const notificationSchema = new Schema(
             type: Schema.Types.ObjectId,
             ref: 'User',
             required: true,
-            index: true // Thêm chỉ mục
+            index: true // Add index for optimized queries
         },
         sender: {
             type: Schema.Types.ObjectId,
@@ -22,10 +22,18 @@ const notificationSchema = new Schema(
             type: Schema.Types.ObjectId,
             ref: 'Question'
         },
+        course: { // Fix typo here
+            type: Schema.Types.ObjectId,
+            ref: 'Course'
+        },
         comment: {
             type: Schema.Types.ObjectId,
             ref: 'Comment',
             required: false
+        },
+        rating: {
+            type: Schema.Types.ObjectId,
+            ref: 'Rating'
         },
         message: {
             type: String,
@@ -39,7 +47,7 @@ const notificationSchema = new Schema(
     { timestamps: true }
 );
 
-// Tạo chỉ mục tổng hợp
+// Create a compound index for recipient and createdAt
 notificationSchema.index({ recipient: 1, createdAt: -1 });
 
 module.exports = mongoose.model('Notification', notificationSchema);
